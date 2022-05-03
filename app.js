82abde724d3cc578fd5d9658d795e57c4c6147ba
Rick Ertl <ertl.rick@gmail.com>
2022-05-03 16:42:56 -0500

    board now not playable after winner

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ const renderBoard = (function () {
   // const cleanArray = plays.filter(function () {
   //   return true;
   // });
+
+  // game play
   for (let i = 0; i < plays.length; i++) {
     let cell = document.createElement("div");
     cell.classList.add(i);
@@ -52,41 +54,53 @@ const renderBoard = (function () {
       }
     });
   }
-})();
 
-// check for winner
-const winnerCheck = function () {
-  if (
-    // horizontal checks
-    (Object.hasOwn(plays, 0) && // makes sure not empty
-      plays[0] === plays[1] &&
-      plays[1] === plays[2]) ||
-    (Object.hasOwn(plays, 3) && // makes sure not empty
-      plays[3] === plays[4] &&
-      plays[4] === plays[5]) ||
-    (Object.hasOwn(plays, 6) && // makes sure not empty
-      plays[6] === plays[7] &&
-      plays[7] === plays[8]) ||
-    // vertical checks
-    (Object.hasOwn(plays, 0) && // makes sure not empty
-      plays[0] === plays[3] &&
-      plays[3] === plays[6]) ||
-    (Object.hasOwn(plays, 1) && // makes sure not empty
-      plays[1] === plays[4] &&
-      plays[4] === plays[7]) ||
-    (Object.hasOwn(plays, 2) && // makes sure not empty
-      plays[2] === plays[5] &&
-      plays[5] === plays[8]) ||
-    // diagonal checks
-    (Object.hasOwn(plays, 0) && // makes sure not empty
-      plays[0] === plays[4] &&
-      plays[4] === plays[8]) ||
-    (Object.hasOwn(plays, 6) && // makes sure not empty
-      plays[6] === plays[4] &&
-      plays[4] === plays[2])
-  ) {
-    setTimeout(function () {
-      alert("Winner!");
-    }, 500);
+  // check for winner
+  function winnerCheck() {
+    if (
+      // horizontal checks
+      (Object.hasOwn(plays, 0) && // makes sure not empty
+        plays[0] === plays[1] &&
+        plays[1] === plays[2]) ||
+      (Object.hasOwn(plays, 3) && // makes sure not empty
+        plays[3] === plays[4] &&
+        plays[4] === plays[5]) ||
+      (Object.hasOwn(plays, 6) && // makes sure not empty
+        plays[6] === plays[7] &&
+        plays[7] === plays[8]) ||
+      // vertical checks
+      (Object.hasOwn(plays, 0) && // makes sure not empty
+        plays[0] === plays[3] &&
+        plays[3] === plays[6]) ||
+      (Object.hasOwn(plays, 1) && // makes sure not empty
+        plays[1] === plays[4] &&
+        plays[4] === plays[7]) ||
+      (Object.hasOwn(plays, 2) && // makes sure not empty
+        plays[2] === plays[5] &&
+        plays[5] === plays[8]) ||
+      // diagonal checks
+      (Object.hasOwn(plays, 0) && // makes sure not empty
+        plays[0] === plays[4] &&
+        plays[4] === plays[8]) ||
+      (Object.hasOwn(plays, 6) && // makes sure not empty
+        plays[6] === plays[4] &&
+        plays[4] === plays[2])
+    ) {
+      setTimeout(function () {
+        alert("Winner!");
+        renderUnplayable();
+      }, 500);
+    }
   }
-};
+
+  // stop board from playable
+  function renderUnplayable() {
+    container.textContent = "";
+    for (let i = 0; i < plays.length; i++) {
+      let finalCell = document.createElement("div");
+      finalCell.classList.add(i);
+      finalCell.textContent = plays[i];
+      container.appendChild(finalCell);
+    }
+  }
+})();
