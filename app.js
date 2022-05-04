82c290914611fdcaf2ab6d910eebaa3770a526e6
Rick Ertl <ertl.rick@gmail.com>
2022-05-04 09:27:22 -0500

    refactored to object literal module pattern

--- app.js
+++ app.js
@@ -10,85 +10,84 @@
 //   return { getName, getWeapon };
 // };
 
-const renderBoard = (function () {
-  let plays = [];
-  plays.length = 9;
-  let weapon = "X";
-  let gameOn = true;
+(function () {
+  const gameboard = {
+    plays: new Array(9),
+    weapon: "X",
+    gameOn: true,
+    init: function () {
+      this.cacheDom();
+      this.render();
+      this.bindEvents();
+    },
+    cacheDom: function () {
+      this.container = document.querySelector(".gameboard");
+    },
+    render: function () {
+      this.container.textContent = "";
+      for (let i = 0; i < this.plays.length; i++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("data-id", i);
+        cell.textContent = this.plays[i];
+        this.container.appendChild(cell);
+      }
+    },
+    bindEvents: function () {
+      if (this.gameOn === true) {
+        const cells = this.container.querySelectorAll("div");
+        cells.forEach((cell) => {
+          cell.addEventListener("click", this.addPlay.bind(this));
+        });
+      }
+    },
+    addPlay: function (event) {
+      if (event.target.textContent === "") {
+        const index = event.target.getAttribute("data-id");
+        this.plays.splice(index, 1, this.weapon);
+      }
+      this.render();
+      this.winnerCheck(this.weapon);
+      this.bindEvents();
+      // ready weapon for next play
+      this.weapon === "X" ? (this.weapon = "O") : (this.weapon = "X");
+    },
+    winnerCheck: function (weapon) {
+      if (
+        // horizontal checks
+        (Object.hasOwn(this.plays, 0) && // makes sure not empty
+          this.plays[0] === this.plays[1] &&
+          this.plays[1] === this.plays[2]) ||
+        (Object.hasOwn(this.plays, 3) && // makes sure not empty
+          this.plays[3] === this.plays[4] &&
+          this.plays[4] === this.plays[5]) ||
+        (Object.hasOwn(this.plays, 6) && // makes sure not empty
+          this.plays[6] === this.plays[7] &&
+          this.plays[7] === this.plays[8]) ||
+        // vertical checks
+        (Object.hasOwn(this.plays, 0) && // makes sure not empty
+          this.plays[0] === this.plays[3] &&
+          this.plays[3] === this.plays[6]) ||
+        (Object.hasOwn(this.plays, 1) && // makes sure not empty
+          this.plays[1] === this.plays[4] &&
+          this.plays[4] === this.plays[7]) ||
+        (Object.hasOwn(this.plays, 2) && // makes sure not empty
+          this.plays[2] === this.plays[5] &&
+          this.plays[5] === this.plays[8]) ||
+        // diagonal checks
+        (Object.hasOwn(this.plays, 0) && // makes sure not empty
+          this.plays[0] === this.plays[4] &&
+          this.plays[4] === this.plays[8]) ||
+        (Object.hasOwn(this.plays, 6) && // makes sure not empty
+          this.plays[6] === this.plays[4] &&
+          this.plays[4] === this.plays[2])
+      ) {
+        setTimeout(function () {
+          alert(`${weapon} wins!`);
+        }, 500);
+        this.gameOn = false;
+      }
+    },
+  };
 
-  // cache gameboard
-  const container = document.querySelector(".gameboard");
-
-  // initialize game
-  render();
-
-  // render from plays array
-  function render() {
-    container.textContent = "";
-    for (let i = 0; i < plays.length; i++) {
-      let cell = document.createElement("div");
-      cell.setAttribute("data-id", i);
-      cell.textContent = plays[i];
-      container.appendChild(cell);
-    }
-    if (gameOn) {
-      playWatcher();
-    }
-  }
-
-  // watch for clicks
-  function playWatcher() {
-    const cells = container.querySelectorAll("div");
-    cells.forEach((cell) => {
-      cell.addEventListener("click", () => {
-        if (cell.textContent === "") {
-          const target = cell.getAttribute("data-id");
-          plays.splice(target, 1, weapon);
-          render();
-          winnerCheck(weapon);
-          // ready weapon for next play
-          weapon === "X" ? (weapon = "O") : (weapon = "X");
-        }
-      });
-    });
-  }
-
-  // check for winner
-  function winnerCheck(weapon) {
-    if (
-      // horizontal checks
-      (Object.hasOwn(plays, 0) && // makes sure not empty
-        plays[0] === plays[1] &&
-        plays[1] === plays[2]) ||
-      (Object.hasOwn(plays, 3) && // makes sure not empty
-        plays[3] === plays[4] &&
-        plays[4] === plays[5]) ||
-      (Object.hasOwn(plays, 6) && // makes sure not empty
-        plays[6] === plays[7] &&
-        plays[7] === plays[8]) ||
-      // vertical checks
-      (Object.hasOwn(plays, 0) && // makes sure not empty
-        plays[0] === plays[3] &&
-        plays[3] === plays[6]) ||
-      (Object.hasOwn(plays, 1) && // makes sure not empty
-        plays[1] === plays[4] &&
-        plays[4] === plays[7]) ||
-      (Object.hasOwn(plays, 2) && // makes sure not empty
-        plays[2] === plays[5] &&
-        plays[5] === plays[8]) ||
-      // diagonal checks
-      (Object.hasOwn(plays, 0) && // makes sure not empty
-        plays[0] === plays[4] &&
-        plays[4] === plays[8]) ||
-      (Object.hasOwn(plays, 6) && // makes sure not empty
-        plays[6] === plays[4] &&
-        plays[4] === plays[2])
-    ) {
-      setTimeout(function () {
-        alert(`${weapon} wins!`);
-        gameOn = false;
-        render();
-      }, 500);
-    }
-  }
+  gameboard.init();
 })();
