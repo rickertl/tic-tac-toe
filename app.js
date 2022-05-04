1b9c60e37b3b247ed0065ff9e20493deba46f2cf
Rick Ertl <ertl.rick@gmail.com>
2022-05-04 16:07:38 -0500

    refactored gameboard as revealing module pattern

--- app.js
+++ app.js
@@ -10,107 +10,110 @@
 //   return { getName, getWeapon };
 // };
 
-(function () {
-  const gameboard = {
-    plays: new Array(9),
-    weapon: "X",
-    gameOn: true,
-    resetButton: document.createElement("button"),
-    init: function () {
-      this.cacheDom();
-      this.render();
-      this.bindEvents();
-    },
-    cacheDom: function () {
-      this.container = document.querySelector(".gameboard");
-    },
-    render: function () {
-      this.container.textContent = "";
-      for (let i = 0; i < this.plays.length; i++) {
-        let cell = document.createElement("div");
-        cell.setAttribute("data-id", i);
-        cell.textContent = this.plays[i];
-        this.container.appendChild(cell);
-      }
-      if (this.gameOn === false) {
-        this.resetButton.textContent = "Play Again?";
-        this.container.appendChild(this.resetButton);
-      }
-    },
-    bindEvents: function () {
-      if (this.gameOn === true) {
-        const cells = this.container.querySelectorAll("div");
-        cells.forEach((cell) => {
-          cell.addEventListener("click", this.addPlay.bind(this));
-        });
-      } else {
-        this.resetButton.addEventListener("click", this.resetPlay.bind(this));
-      }
-    },
-    addPlay: function (event) {
-      if (event.target.textContent === "") {
-        const index = event.target.getAttribute("data-id");
-        this.plays.splice(index, 1, this.weapon);
-        this.winnerCheck(this.weapon);
-        // ready weapon for next play
-        this.weapon === "X" ? (this.weapon = "O") : (this.weapon = "X");
-      }
-      this.render();
-      this.bindEvents();
-    },
-    resetPlay: function () {
-      this.container.classList.remove("gameover");
-      this.plays = new Array(9);
-      this.weapon = "X";
-      this.gameOn = true;
-      this.render();
-      this.bindEvents();
-    },
-    winnerCheck: function (weapon) {
-      if (
-        // horizontal checks
-        (Object.hasOwn(this.plays, 0) && // makes sure not empty
-          this.plays[0] === this.plays[1] &&
-          this.plays[1] === this.plays[2]) ||
-        (Object.hasOwn(this.plays, 3) && // makes sure not empty
-          this.plays[3] === this.plays[4] &&
-          this.plays[4] === this.plays[5]) ||
-        (Object.hasOwn(this.plays, 6) && // makes sure not empty
-          this.plays[6] === this.plays[7] &&
-          this.plays[7] === this.plays[8]) ||
-        // vertical checks
-        (Object.hasOwn(this.plays, 0) && // makes sure not empty
-          this.plays[0] === this.plays[3] &&
-          this.plays[3] === this.plays[6]) ||
-        (Object.hasOwn(this.plays, 1) && // makes sure not empty
-          this.plays[1] === this.plays[4] &&
-          this.plays[4] === this.plays[7]) ||
-        (Object.hasOwn(this.plays, 2) && // makes sure not empty
-          this.plays[2] === this.plays[5] &&
-          this.plays[5] === this.plays[8]) ||
-        // diagonal checks
-        (Object.hasOwn(this.plays, 0) && // makes sure not empty
-          this.plays[0] === this.plays[4] &&
-          this.plays[4] === this.plays[8]) ||
-        (Object.hasOwn(this.plays, 6) && // makes sure not empty
-          this.plays[6] === this.plays[4] &&
-          this.plays[4] === this.plays[2])
-      ) {
-        this.gameOver("regular", weapon, this.container);
-      } else if (!this.plays.includes(undefined)) {
-        this.gameOver("tie", weapon, this.container);
-      }
-    },
-    gameOver: function (result, weapon, container) {
-      if (result === "regular") {
-        container.setAttribute("data-message", `${weapon} wins!`);
-      } else {
-        container.setAttribute("data-message", "Tie!");
-      }
-      container.classList.add("gameover");
-      this.gameOn = false;
-    },
-  };
+// Revealing Module Pattern of gameboard
+const gameboard = (() => {
+  let plays = new Array(9);
+  let weapon = "X";
+  let gameOn = true;
+  const resetButton = document.createElement("button");
 
-  gameboard.init();
+  // cache DOM
+  const container = document.querySelector(".gameboard");
+
+  _render();
+  _bindEvents();
+
+  function _render() {
+    container.textContent = "";
+    for (let i = 0; i < plays.length; i++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("data-id", i);
+      cell.textContent = plays[i];
+      container.appendChild(cell);
+    }
+    if (!gameOn) {
+      resetButton.textContent = "Play Again?";
+      container.appendChild(resetButton);
+    }
+  }
+
+  function _bindEvents() {
+    if (gameOn) {
+      const cells = container.querySelectorAll("div");
+      cells.forEach((cell) => {
+        cell.addEventListener("click", _addPlay);
+      });
+    } else {
+      resetButton.addEventListener("click", _resetPlay);
+    }
+  }
+
+  function _addPlay(event) {
+    if (event.target.textContent === "") {
+      const index = event.target.getAttribute("data-id");
+      plays.splice(index, 1, weapon);
+      _winnerCheck(weapon);
+      // ready weapon for next play
+      weapon === "X" ? (weapon = "O") : (weapon = "X");
+    }
+    _render();
+    _bindEvents();
+  }
+
+  function _resetPlay() {
+    container.classList.remove("gameover");
+    plays = new Array(9);
+    weapon = "X";
+    gameOn = true;
+    _render();
+    _bindEvents();
+  }
+
+  function _winnerCheck() {
+    if (
+      // horizontal checks
+      (Object.hasOwn(plays, 0) && // makes sure not empty
+        plays[0] === plays[1] &&
+        plays[1] === plays[2]) ||
+      (Object.hasOwn(plays, 3) && // makes sure not empty
+        plays[3] === plays[4] &&
+        plays[4] === plays[5]) ||
+      (Object.hasOwn(plays, 6) && // makes sure not empty
+        plays[6] === plays[7] &&
+        plays[7] === plays[8]) ||
+      // vertical checks
+      (Object.hasOwn(plays, 0) && // makes sure not empty
+        plays[0] === plays[3] &&
+        plays[3] === plays[6]) ||
+      (Object.hasOwn(plays, 1) && // makes sure not empty
+        plays[1] === plays[4] &&
+        plays[4] === plays[7]) ||
+      (Object.hasOwn(plays, 2) && // makes sure not empty
+        plays[2] === plays[5] &&
+        plays[5] === plays[8]) ||
+      // diagonal checks
+      (Object.hasOwn(plays, 0) && // makes sure not empty
+        plays[0] === plays[4] &&
+        plays[4] === plays[8]) ||
+      (Object.hasOwn(plays, 6) && // makes sure not empty
+        plays[6] === plays[4] &&
+        plays[4] === plays[2])
+    ) {
+      _gameOver("regular");
+    } else if (!plays.includes(undefined)) {
+      _gameOver("tie");
+    }
+  }
+
+  function _gameOver(result) {
+    if (result === "regular") {
+      container.setAttribute("data-message", `${weapon} wins!`);
+    } else {
+      container.setAttribute("data-message", "Tie!");
+    }
+    container.classList.add("gameover");
+    gameOn = false;
+  }
 })();
+
+// refactoring to revealing module pattern
